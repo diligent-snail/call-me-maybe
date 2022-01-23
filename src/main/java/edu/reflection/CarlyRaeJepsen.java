9a6ca82582bdab7f653d1b9c1809906e5cbd1b91
Denis Zavedeev <happy96538@gmail.com>
2022-01-23 22:55:33 +0300

    Initial commit

---
+++ src/main/java/edu/reflection/CarlyRaeJepsen.java
@@ -0,0 +1,27 @@
+package edu.reflection;
+
+public class CarlyRaeJepsen {
+	@Order(1)
+	@CallMe
+	void sayHello() {
+		System.out.println("Hello");
+	}
+
+	@Order(2)
+	@CallMe
+	void hereIsMyNumber() {
+		System.out.println("Here's my number");
+	}
+
+	@Order(3)
+	@CallMe
+	void soCallMe() {
+		System.out.println("So call me");
+	}
+
+	@Order(4)
+	@CallMe(maybe = false)
+	void maybe() {
+		System.out.println("Maybe");
+	}
+}
